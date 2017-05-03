d6c6a576be0c4be05a31f3c551d3048c27cb4ad7
Janis Streib <janis@dogcraft.de>
2017-05-03 13:19:32 +0200

    ADD: mock server for the badge for easier web dev

---
+++ default-rom/mock-badge-server.py
@@ -0,0 +1,21 @@
+from flask import Flask, redirect
+from time import sleep
+import json
+
+app = Flask(__name__, static_url_path='', static_folder='data/system/web')
+
+
+@app.route('/')
+def index():
+    return redirect('/index.html')
+
+
+@app.route('/api/scan')
+def scan():
+    # Simulate scan
+    sleep(5)
+    return json.dumps([{'ssid': 'Net' + str(i), 'rssi': '-20', 'encType': 4 if i%2 == 0 else 7} for i in range(1, 10)])
+
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=5000, threaded=False, debug=True)
