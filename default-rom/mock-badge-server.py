ea0bc37820c35a7af87c240776ab7d0939e2951d
Janis Streib <janis@dogcraft.de>
2017-05-03 14:13:59 +0200

    ADD: mock: connect check

--- default-rom/mock-badge-server.py
+++ default-rom/mock-badge-server.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect
+from flask import Flask, redirect, request
 from time import sleep
 import json
 
@@ -14,7 +14,19 @@ def index():
 def scan():
     # Simulate scan
     sleep(5)
-    return json.dumps([{'id': i, 'ssid': 'Net' + str(i), 'rssi': '-20', 'encType': 4 if i%2 == 0 else 7} for i in range(0, 10)])
+    return json.dumps(
+        [{'id': i, 'ssid': 'Net' + str(i), 'rssi': '-20', 'encType': 4 if i % 2 == 0 else 7} for i in range(0, 10)])
+
+
+@app.route('/api/conf-wifi', methods=['POST'])
+def conf_wifi():
+    # Simulate connect
+    sleep(5)
+    pw = request.form['pw']
+    id = int(request.form['net'])
+    if pw == "correct" or id % 2 != 0:
+        return "true"
+    return "false"
 
 
 if __name__ == '__main__':
