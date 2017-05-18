3f1bd16800f9eb64baf3320970c56ef4bdd3c500
Anton Schirg <anton.schirg@posteo.de>
2017-05-18 23:13:30 +0200

    Move notification data handling into new file

---
+++ default-rom/notification_db.h
@@ -0,0 +1,31 @@
+// vim: noai:ts=2:sw=2
+#pragma once
+
+#include <Arduino.h>
+#include <FS.h>
+#include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+
+struct Notification {
+  unsigned int id;
+  String summary;
+  String description;
+  String location;
+  uint64_t valid_from;
+  uint64_t valid_to;
+};
+
+enum class NotificationState {
+  SCHEDULED,
+  ACTIVE,
+  NOTIFIED,
+  DISMISSED
+};
+
+Notification * getAnyActiveNotification();
+
+extern unsigned long lastNotificationPull;
+
+void deleteTimestampFiles();
+
+void pullNotifications();
