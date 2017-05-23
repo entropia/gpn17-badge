68076576a8e3a7cc0546c760d62c585d22e0f4a9
Janis Streib <janis@dogcraft.de>
2017-05-24 00:28:30 +0200

    UPD: delted factory rest menu entry

--- default-rom/default-rom.ino
+++ default-rom/default-rom.ino
@@ -197,7 +197,6 @@ void setup() {
       ui->setTheme(new ThemeDark());
       isDark = true;
     }
-    mainMenu->addMenuItem(new MenuItem("Factory reset", []() {}));
     ui->open(mainMenu);
     status->updateBat(badge.getBatVoltage());
     int wStat = WiFi.status();
